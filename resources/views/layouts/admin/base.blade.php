94fdb0876e8d971ef4739b28365aad898c8d3cb2
GuilhermeBobsin <gbobsin3@gmail.com>
2026-08-29 18:20:38 -0300

    Adiciona arquivo base.blade.php com estrutura HTML inicial e inclusão de cabeçalho e rodapé

---
+++ resources/views/layouts/admin/base.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AgroNexo - Gestão Agrícola</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/core@1.4.0/dist/css/tabler.min.css" />
+</head>
+<body>
+    @include('layouts.admin.header')
+        @yield('content')
+    @include('layouts.admin.footer')
+    <script src="https://cdn.jsdelivr.net/npm/@tabler/core@1.4.0/dist/js/tabler.min.js"></script>
+</body>
+</html>
