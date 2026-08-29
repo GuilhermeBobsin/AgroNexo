ea6f544acd7a2a088e73b799e9d89ee97019adba
GuilhermeBobsin <gbobsin3@gmail.com>
2026-08-29 19:38:43 -0300

    Remove comentários desnecessários e organiza a criação da tabela 'propriedades'

--- database/migrations/2026_08_29_223720_create_propriedades_table.php
+++ database/migrations/2026_08_29_223720_create_propriedades_table.php
@@ -6,20 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('propriedades', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->string('responsavel')->nullable();
+            $table->string('localizacao')->nullable();
+            $table->string('latitude', 10, 7)->nullable();
+            $table->string('longitude', 10, 7)->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('propriedades');
