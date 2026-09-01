ee4f2c5c3d3b3a0242de3938f809c8e768521985
GuilhermeBobsin <gbobsin3@gmail.com>
2026-09-01 16:40:06 -0300

    Adiciona rotas para gerenciamento de usuários e reorganiza estrutura de rotas administrativas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
+use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Agronomo\DashboardController as AgronomoDashboardController;
 use App\Http\Controllers\Operador\DashboardController as OperadorDashboardController;
 use App\Http\Controllers\ProfileController;
@@ -10,25 +11,32 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
     Route::get('/dashboard', function () {
-        return redirect()->route('dashboard.' . auth()->user()->perfil);
+        return redirect()->route(
+            'dashboard.' . auth()->user()->perfil
+        );
     })->name('dashboard');
 
     Route::middleware('perfil:admin')->get('/dashboard/admin', [AdminDashboardController::class, 'index'])->name('dashboard.admin');
     Route::middleware('perfil:agronomo')->get('/dashboard/agronomo', [AgronomoDashboardController::class, 'index'])->name('dashboard.agronomo');
     Route::middleware('perfil:operador')->get('/dashboard/operador', [OperadorDashboardController::class, 'index'])->name('dashboard.operador');
 
-    Route::get('/admin/usuarios', [App\Http\Controllers\Admin\UserController::class, 'index'])->name('admin.usuarios.index');
-    
+    Route::prefix('admin')->name('admin.')->middleware('perfil:admin')->group(function () {
+        Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
+        Route::get('/usuarios', [UserController::class, 'index'])->name('usuarios.index');
+    });
+
+    Route::prefix('agronomo')->name('agronomo.')->middleware('perfil:agronomo')->group(function () {
+        Route::get('/dashboard', [AgronomoDashboardController::class, 'index'])->name('dashboard');
+    });
+
+    Route::prefix('operador')->name('operador.')->middleware('perfil:operador')->group(function () {
+        Route::get('/dashboard', [OperadorDashboardController::class, 'index'])->name('dashboard');
+    });
 });
 
 require __DIR__ . '/auth.php';
