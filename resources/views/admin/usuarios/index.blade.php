f77bc4de2806847e149d38b68837a103503bb10a
GuilhermeBobsin <gbobsin3@gmail.com>
2026-09-01 20:30:08 -0300

    Ajusta rota para a criação de novos usuários na listagem

--- resources/views/admin/usuarios/index.blade.php
+++ resources/views/admin/usuarios/index.blade.php
@@ -10,7 +10,7 @@
                 </div>
                 <div class="col-auto ms-auto d-print-none">
                     <div class="d-flex">
-                        <a href="#" class="btn btn-primary btn-3">
+                        <a href="{{ route('admin.usuarios.create') }}" class="btn btn-primary btn-3">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                 stroke-linejoin="round" class="icon icon-2">
