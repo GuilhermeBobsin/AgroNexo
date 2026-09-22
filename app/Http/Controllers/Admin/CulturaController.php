89c25110c1e21884e528a4da700388ea934fa3a8
GuilhermeBobsin <gbobsin3@gmail.com>
2026-09-22 01:04:23 -0300

    Adiciona controlador e rota para listar culturas

--- app/Http/Controllers/Admin/CulturaController.php
+++ app/Http/Controllers/Admin/CulturaController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class CulturaController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('admin.culturas.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\CulturaController;
 use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 use App\Http\Controllers\Admin\PropriedadeController;
 use App\Http\Controllers\Admin\UserController;
@@ -28,51 +29,30 @@ Route::middleware('auth')->group(function () {
     Route::middleware('perfil:operador')->get('/dashboard/operador', [OperadorDashboardController::class, 'index'])->name('dashboard.operador');
 
     Route::prefix('admin')->name('admin.')->middleware('perfil:admin')->group(function () {
+        //dashboard e usuarios
         Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
         Route::get('/usuarios', [UserController::class, 'index'])->name('usuarios.index');
         Route::get('/usuarios/create', [UserController::class, 'create'])->name('usuarios.create');
         Route::post('/usuarios', [UserController::class, 'store'])->name('usuarios.store');
 
-
+        //propriedades
         Route::get('/propriedades', [PropriedadeController::class, 'index'])->name('propriedades.index');
         Route::get('/propriedades/create', [PropriedadeController::class, 'create'])->name('propriedades.create');
         Route::post('/propriedades', [PropriedadeController::class, 'store'])->name('propriedades.store');
         Route::get('/propriedades/{propriedade}', [PropriedadeController::class, 'show'])->name('propriedades.show');
 
-        Route::get(
-            '/propriedades/{propriedade}/talhoes',
-            [TalhaoController::class, 'index']
-        )->name('propriedades.talhoes.index');
+        //talhoes
+        Route::get('/propriedades/{propriedade}/talhoes',[TalhaoController::class, 'index'])->name('propriedades.talhoes.index');
+        Route::get('/propriedades/{propriedade}/talhoes/create',[TalhaoController::class, 'create'])->name('propriedades.talhoes.create');
+        Route::post('/propriedades/{propriedade}/talhoes',[TalhaoController::class, 'store'])->name('propriedades.talhoes.store');
+        Route::get('/propriedades/{propriedade}/talhoes/{talhao}',[TalhaoController::class, 'show'])->name('propriedades.talhoes.show');
+        Route::get('/propriedades/{propriedade}/talhoes/{talhao}/edit',[TalhaoController::class, 'edit'])->name('propriedades.talhoes.edit');
+        Route::put('/propriedades/{propriedade}/talhoes/{talhao}',[TalhaoController::class, 'update'])->name('propriedades.talhoes.update');
+        Route::delete('/propriedades/{propriedade}/talhoes/{talhao}',[TalhaoController::class, 'destroy'])->name('propriedades.talhoes.destroy');
 
-        Route::get(
-            '/propriedades/{propriedade}/talhoes/create',
-            [TalhaoController::class, 'create']
-        )->name('propriedades.talhoes.create');
+        //culturas
+        Route::get('/culturas', [CulturaController::class, 'index'])->name('culturas.index');
 
-        Route::post(
-            '/propriedades/{propriedade}/talhoes',
-            [TalhaoController::class, 'store']
-        )->name('propriedades.talhoes.store');
-
-        Route::get(
-            '/propriedades/{propriedade}/talhoes/{talhao}',
-            [TalhaoController::class, 'show']
-        )->name('propriedades.talhoes.show');
-
-        Route::get(
-            '/propriedades/{propriedade}/talhoes/{talhao}/edit',
-            [TalhaoController::class, 'edit']
-        )->name('propriedades.talhoes.edit');
-
-        Route::put(
-            '/propriedades/{propriedade}/talhoes/{talhao}',
-            [TalhaoController::class, 'update']
-        )->name('propriedades.talhoes.update');
-
-        Route::delete(
-            '/propriedades/{propriedade}/talhoes/{talhao}',
-            [TalhaoController::class, 'destroy']
-        )->name('propriedades.talhoes.destroy');
     });
 
     Route::prefix('agronomo')->name('agronomo.')->middleware('perfil:agronomo')->group(function () {
